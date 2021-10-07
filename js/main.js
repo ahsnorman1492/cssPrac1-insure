5eb7b623de5c4c8e3b605abc0756d1ce6c0ebdc1
Norman <ahs.norman@gmail.com>
2021-10-07 23:51:46 +0800

    finished nav component, added icon__image

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ collapsibles.forEach((item) =>
 
 function chngimg() {
   let img = document.getElementById("navobject").src;
+  // console.log(img);
+  // console.log(document.getElementById("navobject"));
   if (img.indexOf("/images/icon-hamburger.svg") != -1) {
     document.getElementById("navobject").src = "/images/icon-close.svg";
   } else {
